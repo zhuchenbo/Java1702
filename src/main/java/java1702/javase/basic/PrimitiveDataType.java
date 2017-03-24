579dba46a3354c0532f275e18f6a0ee113fe1161
Administrator <749822145@qq.com>
2017-03-24 14:19:26 +0800

    Initial commit

---
+++ src/main/java/java1702/javase/basic/PrimitiveDataType.java
@@ -0,0 +1,19 @@
+package java1702.javase.basic;
+
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+       boolean b = true;
+
+       byte b1 = -128;//最大最小值｛-128，127｝
+        char c = 65535;//最大最小值｛0，65535｝
+        short s = 32767;//最大最小值｛-32768，32767｝
+        int i = 2147483647;//最大最小值｛-2147483648，2147483647｝
+        long l = 1L;//最大最小值｛-9223372036854775808,9223372036854775807｝
+        float f = 1.1f;//如果报错了 一定要报出的是什么错误
+        double d = 2.2;
+//        自动修复快捷键 alt+enter
+    }
+}
