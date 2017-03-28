31fcfbe0bb055fd35cca208fba78cb22f70a976b
Administrator <749822145@qq.com>
2017-03-28 09:19:20 +0800

    Update

--- src/main/java/java1702/javase/collection/_2DArray.java
+++ src/main/java/java1702/javase/collection/_2DArray.java
@@ -2,12 +2,20 @@ package java1702.javase.collection;
 
 import sun.misc.PostVMInitHook;
 
+import javax.sound.midi.Soundbank;
+
 /**
  * Created by xiaozhu
  * on 2017/3/28
  */
 public class _2DArray {
     public static void main(String[] args) {
-
+        int[][] ints = new int[3][5];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(ints[i][j]+"\t");
+            }
+            System.out.println();
+        }
     }
 }
