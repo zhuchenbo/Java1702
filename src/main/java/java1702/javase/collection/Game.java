a7d64a20c60bb836c9e3a129b6fbf78b227fc60b
Administrator <749822145@qq.com>
2017-03-28 11:02:34 +0800

    Update

--- src/main/java/java1702/javase/collection/Game.java
+++ src/main/java/java1702/javase/collection/Game.java
@@ -1,9 +1,22 @@
 package java1702.javase.collection;
 
+import com.sun.prism.MediaFrame;
+
 /**
  * Created by xiaozhu
  * on 2017/3/28
  */
 public class Game {
+    private static final char[] COLORS = {'A', 'B', 'C', 'D', 'E', 'F'};
 
+    public static void main(String[] args) {
+        char[][] square = new char[10][10];
+        for (int i = 0; i < square.length; i++) {
+            for (int j = 0; j < square[i].length; j++) {
+                square[i][j] = COLORS[(int) (Math.random() * 6)];
+                System.out.print(square[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
