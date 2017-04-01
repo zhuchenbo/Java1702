199b88d4d7998d7925deb579dc8e78eb26ad64bb
Administrator <749822145@qq.com>
2017-04-01 08:01:54 +0800

    Update

--- src/main/java/java1702/javase/collection/ListTest.java
+++ src/main/java/java1702/javase/collection/ListTest.java
@@ -1,6 +1,7 @@
 package java1702.javase.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiaozhu
@@ -21,13 +22,18 @@ public class ListTest {
         System.out.println(strings.remove(1));
         System.out.println(strings.set(0,"test"));
         Object[] objects=strings.toArray();
-        //strings.add("hello");
+        strings.add("hello");
         System.out.println(strings.size());
         System.out.println(strings);
         ArrayList<String> strings1 = new ArrayList<>();
-        strings1.add("hello");
+        //strings1.add("hello");
         strings.removeAll(strings1);
-        System.out.println(strings.size());
         System.out.println(strings);
+        System.out.println(strings.size());
+        strings.trimToSize();
+        System.out.println(strings.size());
+
+        List<String> strings2=strings.subList(1,2);
+        System.out.println(strings2);
     }
 }
